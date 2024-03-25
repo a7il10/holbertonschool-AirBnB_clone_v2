fdabefed53cd8c8a8a7f0a4c43cfa21cb797a423
Ferlandoo <contact@ferlando.me>
2024-03-25 17:50:32 +0100

    update state

--- models/state.py
+++ models/state.py
@@ -1,30 +1,34 @@
-#!/usr/bin/python3
-"""This is the state class"""
-from models.base_model import BaseModel, Base
-from sqlalchemy.orm import relationship
-from sqlalchemy import Column, String
+ #!/usr/bin/python3
+""" State Module for HBNB project """
 import models
-import shlex
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship, backref
+from models.city import City
+from os import getenv
 
 
 class State(BaseModel, Base):
-    """This is the class for State"""
+    """This is the class for State
+    Attributes:
+        name: input name
+    """
     __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade='all, delete, delete-orphan',
-                          backref="state")
 
-    @property
-    def cities(self):
-        var = models.storage.all()
-        lista = []
-        result = []
-        for key in var:
-            city = key.replace('.', ' ')
-            city = shlex.split(city)
-            if (city[0] == 'City'):
-                lista.append(var[key])
-        for elem in lista:
-            if (elem.state_id == self.id):
-                result.append(elem)
-        return (result)
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", cascade="all, delete", backref="state")
+
+    else:
+        name = ""
+
+        @property
+        def cities(self):
+            """ Returns the list of City
+            """
+            the_cities = []
+            for c in models.storage.all(City).values():
+                if c.state_id == self.id:
+                    the_cities.append(c)
+            return the_cities
