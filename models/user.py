272fb3f76646679d21a59e76c6060bce9f46ae14
Ferlandoo <contact@ferlando.me>
2024-03-25 18:59:03 +0100

    update

--- models/user.py
+++ models/user.py
@@ -1,19 +1,24 @@
-from sqlalchemy import Column, String
-from sqlalchemy.ext.declarative import declarative_base
-from models.base_model import BaseModel
-from models.base_model import Base
+#!/usr/bin/python3
+"""This is the user class"""
+from models.base_model import Base, BaseModel
+from sqlalchemy import Column, Integer, String
+import sqlalchemy
+from sqlalchemy.orm import backref, relationship
 from os import getenv
-from sqlalchemy.orm import relationship
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
+    if getenv("HBNB_TYPE_STORAGE") == 'db':
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
-        places = relationship("Place", back_populates="user", cascade="all, delete")
+        reviews = relationship("Review", backref="user",
+                           cascade="all, delete, delete-orphan")
+
+        places = relationship("Place", backref="user",
+                              cascade="all, delete, delete-orphan")
     else:
         email = ""
         password = ""
