8731ede934f677b861ec5e569df3919a8add0efd
Ferlandoo <contact@ferlando.me>
2024-03-25 18:19:11 +0100

    update

--- models/state.py
+++ models/state.py
@@ -11,7 +11,6 @@ from os import getenv
 class State(BaseModel, Base):
     """This is the class for State"""
     __tablename__ = "states"
-
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         name = Column(String(128), nullable=False)
         cities = relationship("City", cascade="all, delete", backref="state")
