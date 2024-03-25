eb2a0615b6d65c15e194dfccf332d490bd48ee2a
Ferlandoo <contact@ferlando.me>
2024-03-25 18:38:13 +0100

    update

--- models/state.py
+++ models/state.py
@@ -9,20 +9,26 @@ from os import getenv
 
 
 class State(BaseModel, Base):
-    """This is the class for State"""
+    """This is the class for State
+    Attributes:
+        name: input name
+    """
     __tablename__ = "states"
+
     if getenv('HBNB_TYPE_STORAGE') == 'db':
+
         name = Column(String(128), nullable=False)
         cities = relationship("City", cascade="all, delete", backref="state")
+
     else:
         name = ""
 
-    @property
-    def cities(self):
-        """ Returns the list of City
-        """
-        the_cities = []
-        for c in models.storage.all(City).values():
-            if c.state_id == self.id:
-                the_cities.append(c)
-        return the_cities
+        @property
+        def cities(self):
+            """ Returns the list of City
+            """
+            the_cities = []
+            for c in models.storage.all(City).values():
+                if c.state_id == self.id:
+                    the_cities.append(c)
+            return the_cities
