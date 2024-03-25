5b9cc28a042892ce355ae1e3bce3ae92550aa162
Ferlandoo <contact@ferlando.me>
2024-03-25 19:01:00 +0100

    update places

--- models/place.py
+++ models/place.py
@@ -1,16 +1,25 @@
 #!/usr/bin/python3
-""" Place Module for HBNB project """
+"""This is the place class"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, Float, ForeignKey
-from os import getenv
+from sqlalchemy import Column, Integer, String, ForeignKey, Float, Table
 from sqlalchemy.orm import relationship
+from os import getenv
+
+
+place_amenity = Table('place_amenity', Base.metadata,
+                      Column('place_id', String(60), ForeignKey('places.id'),
+                             primary_key=True, nullable=False),
+                      Column('amenity_id', String(60),
+                             ForeignKey('amenities.id'),
+                             primary_key=True, nullable=False))
 
 
 class Place(BaseModel, Base):
+    """This is the class for Place"""
     __tablename__ = 'places'
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
-        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    if getenv("HBNB_TYPE_STORAGE") == 'db':
+        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
         name = Column(String(128), nullable=False)
         description = Column(String(1024), nullable=True)
         number_rooms = Column(Integer, nullable=False, default=0)
@@ -19,6 +28,11 @@ class Place(BaseModel, Base):
         price_by_night = Column(Integer, nullable=False, default=0)
         latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
+        amenity_ids = []
+        reviews = relationship("Review", backref="Place",
+                               cascade="all, delete, delete-orphan")
+        amenities = relationship("Amenity", secondary=place_amenity,
+                                 viewonly=False, backref="place_amenities")
     else:
         city_id = ""
         user_id = ""
@@ -30,3 +44,25 @@ class Place(BaseModel, Base):
         price_by_night = 0
         latitude = 0.0
         longitude = 0.0
+        amenity_ids = []
+
+        @property
+        def reviews(self):
+            review_list = []
+            for value in models.storage.all(Review).values():
+                if value.place_id == self.id:
+                    review_list.append(value)
+                return review_list
+
+        @property
+        def amenities(self):
+            amenity_list = []
+            for value in models.storage.all(Amenity).values():
+                if value.place_amenity.place_id == self.id:
+                    amenity_list.append(value)
+                return amenity_list
+
+        @amenities.setter
+        def amenities(self, obj=None):
+            if type(obj) == Amenity:
+                self.amenity_ids.append(obj.id)
