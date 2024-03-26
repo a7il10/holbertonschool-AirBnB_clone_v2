a18f9cefadc6f24cf65d71ef0a86234f7b33957b
Ferlandoo <contact@ferlando.me>
2024-03-26 13:08:55 +0100

    add classes

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -11,6 +11,8 @@ from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
 
+classes = {"Amenity": Amenity, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
 
 class DBStorage:
     """DBStorage class"""
