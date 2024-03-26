68769afa19756cb540beb817faa0e997f9744952
Ferlandoo <contact@ferlando.me>
2024-03-26 11:59:31 +0100

    remove privat line

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -14,7 +14,7 @@ from models.review import Review
 
 class DBStorage:
     """DBStorage class"""
-    __engine = None
+    engine = None
     session = None
 
     def __init__(self):
@@ -24,13 +24,11 @@ class DBStorage:
         mysql_host = getenv('HBNB_MYSQL_HOST')
         mysql_db = getenv('HBNB_MYSQL_DB')
         mysql_env = getenv('HBNB_ENV')
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
+        self.engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
             mysql_user, mysql_pwd, mysql_host, mysql_db), pool_pre_ping=True)
-        Base.metadata.create_all(self.__engine)
+        Base.metadata.create_all(self.engine)
         if mysql_env == 'test':
-            Base.metadata.drop_all(self.__engine)
-        Session = sessionmaker(bind=self.__engine)
-        self.__session = Session()
+            Base.metadata.drop_all(self.engine)
 
     def all(self, cls=None):
         """show all data"""
@@ -38,14 +36,14 @@ class DBStorage:
         if cls:
             if type(cls) is str:
                 cls = eval(cls)
-            query = self.__session.query(cls).all()
+            query = self.session.query(cls).all()
             for data in query:
                 key = "{}.{}".format(type(data).__name__, data.id)
                 elem_dict[key] = data
         else:
             list_cls = [State, City, User, Place, Review, Amenity]
             for clas in list_cls:
-                query = self.__session.query(clas)
+                query = self.session.query(clas)
                 for data in query:
                     key = "{}.{}".format(type(data).__name__, data.id)
                     elem_dict[key] = data
@@ -54,29 +52,29 @@ class DBStorage:
     def new(self, obj):
         """Add the object in the databse"""
         if obj:
-            self.__session.add(obj)
+            self.session.add(obj)
 
     def save(self):
         """Commit all changes of the current
         database session"""
-        self.__session.commit()
+        self.session.commit()
 
     def delete(self, obj=None):
         """Delete from the current database"""
         if obj:
-            self.__session.delete(obj)
+            self.session.delete(obj)
 
     def close(self):
         """Closes Session"""
-        self.__session.close()
+        self.session.close()
 
     def reload(self):
         """Create database in Alchemy"""
-        Base.metadata.create_all(self.__engine)
-        db_fac = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Base.metadata.create_all(self.engine)
+        db_fac = sessionmaker(bind=self.engine, expire_on_commit=False)
         Session = scoped_session(db_fac)
-        self.__session = Session()
+        self.session = Session()
 
     def close(self):
         """Clise session"""
-        self.__session.close()
+        self.session.close()
