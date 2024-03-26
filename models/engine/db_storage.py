aa717b34424038b303e167069f7a4108b8897f05
Ferlandoo <contact@ferlando.me>
2024-03-26 11:55:30 +0100

    update test

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -38,14 +38,14 @@ class DBStorage:
         if cls:
             if type(cls) is str:
                 cls = eval(cls)
-            query = Session().query(cls).all()
+            query = Session.query(cls).all()
             for data in query:
                 key = "{}.{}".format(type(data).__name__, data.id)
                 elem_dict[key] = data
         else:
             list_cls = [State, City, User, Place, Review, Amenity]
             for clas in list_cls:
-                query = Session().query(clas)
+                query = Session.query(clas)
                 for data in query:
                     key = "{}.{}".format(type(data).__name__, data.id)
                     elem_dict[key] = data
