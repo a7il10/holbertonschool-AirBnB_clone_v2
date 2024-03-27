c3395e565f4129f8c711f7f31377ace2499aa4a1
Ferlandoo <contact@ferlando.me>
2024-03-27 11:53:11 +0100

    update pycodestyle

--- models/state.py
+++ models/state.py
@@ -20,7 +20,7 @@ class State(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes state"""
         super().__init__(*args, **kwargs)
-    
+
     if models.storage_type != 'db':
         @property
         def cities(self):
