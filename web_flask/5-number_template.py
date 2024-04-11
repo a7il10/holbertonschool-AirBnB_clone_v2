f1a24d46c1dfefa3a18ad31775711cbd44a21a3c
a7il10 <atilio.sulovari199@gmail.com>
2024-04-11 10:57:34 +0200

    ex 5 update

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,37 +1,65 @@
-from flask import Flask, render_template
+#!/usr/bin/python3
+"""
+Module to initiate a flask app
+"""
+
+
+from flask import Flask
+from flask import render_template
+
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
-def hello():
-    return "Hello HBNB!"
+def hello_hbnb():
+    """
+    Index route
+    """
+    return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    return "HBNB"
+    """
+    HBNB route
+    """
+    return 'HBNB'
+
 
 @app.route('/c/<text>', strict_slashes=False)
-def cisfun(text):
-    text = text.replace("_", " ")
-    return f"C {text}"
+def c_is_fun(text):
+    """
+    Dynamic routing
+    """
+    return 'C ' + text.replace('_', ' ')
 
-@app.route('/python', defaults={"text": "is cool"})
+
+@app.route('/python/')
 @app.route('/python/<text>', strict_slashes=False)
-def pyhtoniscool(text):
-    text = text.replace("_", " ")
-    return f"Python {text}"
+def python_is_cool(text='is cool'):
+    """
+    Dynamic routing
+    """
+    return 'Python ' + text.replace('_', ' ')
 
-@app.route('/number/<int:n>',strict_slashes=False)
-def number_n(n):
-    """func to be executed"""
 
-    return f"{n} is a number"
+@app.route('/number/<int:n>', strict_slashes=False)
+def is_number(n):
+    """
+    Dynamic routing
+    """
+    return str(n) + ' is a number'
 
-@app.route('/number_template/<int:n>',strict_slashes=False)
-def number(n):
-    """func to be executed"""
 
-    return render_template("5-number.html", n=n)
+@app.route('/number_template/<int:n>', strict_slashes=False)
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """
+    Dynamic routing and template rendering
+    """
+    return render_template('5-number.html', n=n)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
